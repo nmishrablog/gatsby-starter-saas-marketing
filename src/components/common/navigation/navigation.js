08b48143958e350cc96f1515819ba70f4e9dad11
nmishrablog <31518505+nmishrablog@users.noreply.github.com>
2019-12-11 15:24:59 +0530

    Update navigation.js

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -15,7 +15,7 @@ import {
   ActionsContainer,
 } from "./style"
 
-const NAV_ITEMS = ["Features", "Product", "Pricing", ""]
+const NAV_ITEMS = ["Features", "Topics", "Pricing", ""]
 
 export default class Navigation extends Component {
   state = {
